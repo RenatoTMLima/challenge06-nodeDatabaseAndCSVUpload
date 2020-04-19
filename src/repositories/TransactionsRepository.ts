235805f03963bdccb943094118cce2aca9b50950
Renato Takao Matura de Lima <renato.lima23@fatec.sp.gov.br>
2020-04-19 00:17:14 -0300

    Developed some part of the challenge

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,28 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const allTransactions = await this.find();
+
+    console.log(allTransactions);
+
+    const income = allTransactions
+      .filter(transaction => transaction.type === 'income')
+      .reduce((totalIncome, transaction) => totalIncome + transaction.value, 0);
+
+    const outcome = allTransactions
+      .filter(transaction => transaction.type === 'outcome')
+      .reduce(
+        (totalOutcome, transaction) => totalOutcome + transaction.value,
+        0,
+      );
+
+    const balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 }
 
